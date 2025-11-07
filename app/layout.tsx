711c464dafb1cfd31f068ebdc5ccf74ce96d6dfc
김우찬 <dnckss1121@gmail.com>
2025-11-07 10:09:37 +0900

    fix :: build 에러 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import { Suspense } from "react";
 import { GtagPageview } from "@/components/gtag-pageview";
 import "./globals.css";
 
@@ -42,7 +43,9 @@ export default function RootLayout({
             gtag('config', 'G-LCGB8H13K7');
           `}
         </Script>
-        <GtagPageview />
+        <Suspense fallback={null}>
+          <GtagPageview />
+        </Suspense>
         {children}
       </body>
     </html>
